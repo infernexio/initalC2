8e9916a380b05b5a1ec4877efb7bfb0eaf97b971
infernexio <sohail382517@gmail.com>
2022-10-30 12:53:23 -0400

    added threading to the c2

--- server.py
+++ server.py
@@ -1,26 +1,37 @@
-import ipaddress
 import socket
-from xmlrpc import server
+import threading
 
 ip_adress = '127.0.0.1'
 port_number = 1234
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+THREADS = []
+CMD_INPUT = []
+CMD_OUTPUT = []
 
-server_socket.bind((ip_adress, port_number))
-
-server_socket.listen(2)
-
-client_socket,adress = server_socket.accept()
-
-print("Client: {}".format(adress))
-msg = client_socket.recv(1024).decode()
-
-while msg != 'quit':
-    print(msg)
-    client_socket.send(msg.encode())
-    msg = client_socket.recv(1024).decode()
+def handle_conection(connection, adress):
+    msg = connection.recv(1024).decode()
+    while msg != 'quit':
+        print(msg)
+        connection.send(msg.encode())
+        msg = connection.recv(1024).decode()
+    close_connection(connection)
 
 
-client_socket.close()
-server_socket.close()
+def close_connection(connection):
+    connection.close()
+
+def init_server():
+    # creating socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip_adress, port_number))
+    server_socket.listen(5)
+    while True:
+        connection,adress = server_socket.accept()
+        # creating threads for each connectoin
+        t = threading.Thread(target=handle_conection, args=(connection,adress))
+        THREADS.append(t)
+        t.start()
+
+
+if __name__ == "__main__":
+    init_server()
---
+++ threading_sample.py
@@ -0,0 +1,13 @@
+import threading;
+import time
+
+def testing():
+    print('this gets executed by {}'.format(threading.current_thread().name))
+    time.sleep(2)
+
+THREADS = []
+
+for i in range(0,5):
+    t = threading.Thread(target=testing)
+    THREADS.append(t)
+    t.start()
